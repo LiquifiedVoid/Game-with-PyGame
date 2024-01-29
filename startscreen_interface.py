2bba2a6f6784048e9259f4c280106b1185434b00
Vincent <vincent.heddergott@gmail.com>
2024-01-29 13:01:34 +0100

    sound :(

--- startscreen_interface.py
+++ startscreen_interface.py
@@ -8,6 +8,24 @@ class Interface_Startscreen():
         self.display_surface = screen
         self.eh = e_h
         self.player = player
+        
+        # # sound
+        # size = 70
+        # self.sounds_on = pygame.image.load("images/level/startscreen/sounds-on.png")
+        # self.sounds_off = pygame.image.load("images/level/startscreen/sounds-off.png")
+        # self.music_on = pygame.image.load("images/level/startscreen/music-on.png")
+        # self.music_off = pygame.image.load("images/level/startscreen/music-off.png")
+        # self.info = pygame.image.load("images/level/startscreen/info.png")
+
+        # self.sounds_on = pygame.transform.scale(self.sounds_on, (size,size))
+        # self.sounds_off = pygame.transform.scale(self.sounds_off, (size,size))
+        # self.music_on = pygame.transform.scale(self.music_on, (size,size))
+        # self.music_off = pygame.transform.scale(self.music_off, (size,size))
+        # self.info = pygame.transform.scale(self.info, (60,60))
+
+        # self.sounds_state = self.sounds_on
+        # self.music_state = self.music_on
+
 
     def get_font(self, size):
         """Gibt eine Schriftart zurück mit gegebener 
@@ -50,6 +68,24 @@ class Interface_Startscreen():
         game_name_rect.center = (center_x, center_y-250 + sin)
         self.display_surface.blit(game_name, game_name_rect)
         
+        # #sound buttons
+        # sounds_on_rect = self.sounds_on.get_rect()
+        # sounds_off_rect = self.sounds_off.get_rect()
+        # music_on_rect = self.music_on.get_rect()
+        # music_off_rect = self.music_off.get_rect()
+        # info_rect = self.info.get_rect()
+        
+        # sounds_on_rect.center = (1870,1040)
+        # sounds_off_rect.center = (1870,1040)
+        # music_on_rect.center = (1780,1040)
+        # music_off_rect.center = (1780,1040)
+        # info_rect.center = (40,1040)
+        
+        
+        
+        # self.display_surface.blit(self.sounds_state, sounds_on_rect)
+        # self.display_surface.blit(self.music_state, music_on_rect)
+        # self.display_surface.blit(self.info, info_rect)
         
         # check clicked
         for event in self.eh.get_events():
@@ -60,3 +96,16 @@ class Interface_Startscreen():
             # run game
             if play_rect.collidepoint(mouse_pos[0], mouse_pos[1]) and event.type == pygame.MOUSEBUTTONUP:
                 return True
+            # #sound
+            # if sounds_on_rect.collidepoint(mouse_pos[0], mouse_pos[1]) and event.type == pygame.MOUSEBUTTONUP and self.sounds_state == self.sounds_on:
+            #     self.sounds_state = self.sounds_off
+            #     pygame.mixer.music.set_volume(0)
+            # elif sounds_off_rect.collidepoint(mouse_pos[0], mouse_pos[1]) and event.type == pygame.MOUSEBUTTONUP and self.sounds_state == self.sounds_off:
+            #     self.sounds_state = self.sounds_on
+            #     pygame.mixer.music.set_volume(0.05)
+                
+            # if music_on_rect.collidepoint(mouse_pos[0], mouse_pos[1]) and event.type == pygame.MOUSEBUTTONUP and self.music_state == self.music_on:
+            #     self.music_state = self.music_off
+            # elif music_off_rect.collidepoint(mouse_pos[0], mouse_pos[1]) and event.type == pygame.MOUSEBUTTONUP and self.music_state == self.music_off:
+            #     self.music_state = self.music_on
+            
